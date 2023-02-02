6a4eb7ec87e617b7c0110ea4f4adee4f44995bb2
Dreamnight1015 <114711729+Dreamnight1015@users.noreply.github.com>
2023-02-03 02:30:39 +0900

    Portfolio

--- Portfolio.js
+++ Portfolio.js
@@ -158,4 +158,37 @@ $(document).ready(function(){
         $('.indexsub li:nth-child(3) .indexls').css('opacity','0');
         $('.indexsub li:nth-child(4) .indexls').css('opacity','0');
     })
+    const $text = document.querySelector(".text");
+
+// 글자 모음 - 개행문자(\n)로 줄바꿈
+const letters = [
+  "안녕하세요. \n 성장하기 위해 노력하는 퍼블리셔 한동규입니다.",
+];
+
+// 글자 입력 속도
+const speed = 100;
+let i = 0;
+
+// 줄바꿈을 위한 <br> 치환
+const changeLineBreak = (letter) => {
+  return letter.map(text => text === "\n" ? "<br>" : text);
+}
+
+// 타이핑 효과
+const typing = async () => {  
+  // 기존코드에서 개행치환코드 추가
+  const letter = changeLineBreak(letters[i].split(""));
+  
+  while (letter.length) {
+    await wait(speed);
+    $text.innerHTML += letter.shift(); 
+  }
+}
+// 딜레이 기능 ( 마이크로초 )
+function wait(ms) {
+  return new Promise(res => setTimeout(res, ms))
+}
+
+// 초기 실행
+setTimeout(typing, 1500);
 });
